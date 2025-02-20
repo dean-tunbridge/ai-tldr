610f422da6727c2e2ba8a1c7bec1762af357fc5e
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-02-20 16:34:12 +1300

    add logo and some styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,11 @@ import { logo } from '../assets'
 
 function Hero() {
   return (
-    <header className="w-full flex justify-center items-center flex-col"></header>
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center flex-col">
+        <img src={logo} alt="sumz-logo" className="w-28" />
+      </nav>
+    </header>
   )
 }
 
