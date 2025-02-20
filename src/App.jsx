92c3f64f88a1ff517a1f6fbbe2df7ac02d287804
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-02-20 16:15:10 +1300

    remove satoshi font. Will have to research using google fonts in tailwind 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import Demo from './components/Demo'
+import Hero from './components/Hero'
+
+import './App.css'
+
 function App() {
-  return <div>App</div>
+  return (
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+    </main>
+  )
 }
 
 export default App
