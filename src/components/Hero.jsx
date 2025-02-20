5fdd33b49735de39fb15ec7a4b2575318c880062
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-02-20 16:36:10 +1300

    add more classes to nav. Stopping for the day

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,8 +3,8 @@ import { logo } from '../assets'
 function Hero() {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center flex-col">
-        <img src={logo} alt="sumz-logo" className="w-28" />
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="sumz-logo" className="w-28 object-contain" />
       </nav>
     </header>
   )
