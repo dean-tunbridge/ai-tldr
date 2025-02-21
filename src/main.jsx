5d46c61e262536d618d1cf057303adebb6038189
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-02-21 16:37:06 +1300

    finsih set up fo store. dev  buiold is broken as the endpoint is not there yet but its expected

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,11 @@ import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 
 import App from './App.jsx'
+import { store } from './services/store.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>
   </StrictMode>
