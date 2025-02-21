cf415dc15481dcce3f2def92c9e11c84dc4babbb
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-02-21 15:47:04 +1300

    add h1 to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,6 +6,11 @@ function Hero() {
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
         <img src={logo} alt="sumz-logo" className="w-28 object-contain" />
       </nav>
+
+      <h1 className="head_text">
+        Summarise Articles with <br />
+        <span>OpenAI GPT-4</span>
+      </h1>
     </header>
   )
 }
