9252524a4d918b557ae654e3f83ac6439b76ed74
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-02-21 15:54:42 +1300

    add structure to demo. Add empty callback function

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,7 +1,15 @@
 import { useState, useEffect } from 'react'
 import { copy, linkIcon, loader, tick } from '../assets'
 function Demo() {
-  return <div>Demo</div>
+  return (
+    <section className="mt-16 w-full max-w-xl">
+      <div className="flex flex-colw-full gap-2">
+        <form
+          className="relative flex justify-center item center"
+          onSubmit={() => {}}></form>
+      </div>
+    </section>
+  )
 }
 
 export default Demo
