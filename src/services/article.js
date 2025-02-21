dc7f6201f39881dba56344d313655fe1fc2297df
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-02-21 16:51:34 +1300

    add base url

--- src/services/article.js
+++ src/services/article.js
@@ -1,25 +1,24 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
 
-
 const options = {
   method: 'GET',
   url: 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize',
   qs: {
     url: 'https://time.com/6266679/musk-ai-open-letter/',
     lang: 'en',
-    engine: '2'
+    engine: '2',
   },
   headers: {
     'x-rapidapi-key': '6415556743msh75bb24354eb7f24p12da11jsnff5d676164e3',
-    'x-rapidapi-host': 'article-extractor-and-summarizer.p.rapidapi.com'
-  }
-};
+    'x-rapidapi-host': 'article-extractor-and-summarizer.p.rapidapi.com',
+  },
+}
 
 export const articleApi = createApi({
   reducerPath: 'articleApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: ''
-  })
+    baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+  }),
   endpoints: (builder) => ({
     getSummary: builder.query({
       query: (params) => 'test',
