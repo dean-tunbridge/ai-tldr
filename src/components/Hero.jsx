58fe78df8ab91e9d421a84df14f090478cc617f2
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-04-01 20:13:20 +1300

    Change text on homepage.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,15 @@
-import { logo } from '../assets'
-
 function Hero() {
   return (
     <header className="w-full flex justify-center items-center flex-col">
-      <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <img src={logo} alt="sumz-logo" className="w-28 object-contain" />
-      </nav>
+      <nav className="flex justify-between items-center w-full mb-10 pt-3"></nav>
 
       <h1 className="head_text">
-        Summarise Articles with <br className="max-md:hidden" />
+        TL;DR with <br className="max-md:hidden" />
         <span className="orange_gradient">OpenAI GPT-4</span>
       </h1>
       <h2 className="desc">
-        Simplify your reading usign this open source article summariser that
-        transforms lengthy articles into clear and concise summaries
+        Blah Blah Blah so what is a Komodo Dragon anyway?
+        <br />
       </h2>
     </header>
   )
