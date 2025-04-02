fdbfd8c5c1909a497bb6dce3747de3ba1b875583
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-04-02 16:46:03 +1300

    fixed loading spinner. Happy with it. Next I will try and add delete function to previous searches

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <div className="p-3 animate-spin drop-shadow-2xl bg-gradient-to-bl from-violet-600 via-purple-500 to-violet-800 md:w-24 md:h-24 h-24 w-24 aspect-square rounded-full">
-      <div className="rounded-full h-full w-full bg-slate-100 dark:bg-zinc-900 background-blur-md" />
+      <div className="rounded-full h-full w-full opacity-50 bg-slate-100 dark:bg-zinc-900 background-blur-md" />
     </div>
   )
 }
