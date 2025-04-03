499b8765e0d06395c3cd46038fb4431bf0a4d0da
Dean Tunbridge <deantunbridgedev@gmail.com>
2025-04-03 20:48:26 +1300

    changed description

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,11 +4,12 @@ function Hero() {
       <nav className="flex justify-between items-center w-full mb-10 pt-3"></nav>
 
       <h1 className="head_text">
-        TL;DR with <br className="max-md:hidden" />
+        TL;DR using <br className="max-md:hidden" />
         <span className="purple_gradient">OpenAI GPT-4</span>
       </h1>
       <h2 className="description">
-        Blah Blah Blah so what exactly is a Komodo Dragon anyway?
+        Paste the URL of an article, blog, or wikipedia page and get a summary
+        in seconds.
         <br />
       </h2>
     </header>
